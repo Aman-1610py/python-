a748c8f63e94aaa30de19f969854fed8452fc8be
Aman-1610py <kamankumar113@gmail.com>
2024-07-20 18:32:27 +0530

    file added till 4

---
+++ 04_operator.py
@@ -0,0 +1,30 @@
+# Arithmetic Operators 
+
+a = 9
+
+b = 9
+
+c = a+b
+
+print(c)
+
+
+# Assigment operator
+
+a  = 9 - 4  #Assign 9-4 in a
+b = 8
+print(a)
+b +=8  #increment the value of b by 8 and then assign it to b
+print(b)
+
+# comparison operator
+
+f = 9>5
+print(f)
+
+e = 5!=5
+print(e)
+
+# logical operator
+w  = False or False
+print(w)
